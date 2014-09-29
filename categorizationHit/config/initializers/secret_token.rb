bd7298ef9e5070e29758f9cd6d7078deac07e31c
William Huang <whuang6@stanford.edu>
2014-09-28 19:48:30 -0700

    initial commit

---
+++ categorizationHit/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CategorizationHit::Application.config.secret_key_base = '7759142b14245b02e439e694666d70a42e8a91a59d5ede7a85b97981d4192199be256600ed474a83a471211424194225cf5f7b0814ad4c7a7b2b04d2efa643c5'
---
+++ categorizationHit/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CategorizationHit::Application.config.session_store :cookie_store, key: '_categorizationHit_session'
